5732c6ebbcf0469958093613010e76a6849ac91b
Rik Goldman <ghoulmann@users.noreply.github.com>
2021-01-15 12:23:44 -0500

    add some output

--- opm/seed_database.py
+++ opm/seed_database.py
@@ -1,12 +1,14 @@
 import shelve
 from input.read_document import Sample
 import pathlib, os
+path = pathlib.Path(__file__).parent.absolute()
+print("Working Direcory" + str(path))
 document = 'util/12thGrade-informationExplanatory.txt'
 
 report = Sample(document)
-path = pathlib.Path(__file__).parent.absolute()
-db = shelve.open(os.path.join(path, 'shelve.db'))
 
+db = shelve.open(os.path.join(path, 'shelve.db'))
+Print("Writing shelve.db to " + str(os.path.join(path, 'shelve.db'))
 db['example'] = report
 
 db.close()
